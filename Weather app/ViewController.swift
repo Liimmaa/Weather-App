7349b38cd63edbaa19da94314c9f02d3cea66966
liimmaa <halimammegwa@gmail.com>
2020-09-13 23:52:23 +0100

    Displaying static data

---
+++ Weather app/ViewController.swift
@@ -0,0 +1,31 @@
+////
+////  ViewController.swift
+////  Weather app
+////
+////  Created by USER on 13/09/2020.
+////  Copyright © 2020 Lima. All rights reserved.
+////
+//
+//import UIKit
+//
+//class ViewController: UIViewController, UITableViewDataSource {
+////    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        return 1
+////    }
+////
+//    
+////    let fruit = ["Apple", "Orange", "Mango"]
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+////        tableView.backgroundColor = UIColor.gray
+//    }
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    if let cell = tableView.dequeueReusableCell(withIdentifier: "weather", for: indexPath) as? WeatherCell{
+//    cell.day?.text = "hello"
+//        
+//        return cell
+//}
+//        return UITableViewCell()
+//}
+//}
---
+++ Weather app/WeatherCell.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherCell.swift
+//  Weather app
+//
+//  Created by USER on 13/09/2020.
+//  Copyright © 2020 Lima. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+//    @IBOutlet weak var minVal: UILabel!
+//    @IBOutlet weak var currentVal: UILabel!
+//    @IBOutlet weak var maxVal: UILabel!
+//    @IBOutlet weak var minLabel: UILabel!
+//    @IBOutlet weak var currentLabel: UILabel!
+//    @IBOutlet weak var maxLabel: UILabel!
+//    @IBOutlet weak var minVal: UILabel!
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var sun: UIImageView!
+    @IBOutlet weak var temp: UILabel!
+    
+    //
+    
+    
+//    @IBOutlet weak var day: UILabel!
+//    @IBOutlet weak var sun: UIImageView!
+//    @IBOutlet weak var temp: UILabel!
+    
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
